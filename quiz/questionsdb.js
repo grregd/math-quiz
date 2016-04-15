550f294d2a523df175c8e4f8ea2e06120c21ef52
Grzegorz Dulewicz <grzegorz.dulewicz@tieto.com>
2016-04-15 11:38:11 +0200

    Questions DB extended with penalty value.

--- quiz/questionsdb.js
+++ quiz/questionsdb.js
@@ -1,6 +1,10 @@
 
-function createItem( aQuery, aPointsMax, aAnswers, aCorrect ) {
-    return { query: aQuery, pointsMax: aPointsMax, answers: aAnswers, correct: aCorrect };
+function createItem( aQuery, aPointsMax, aAnswers, aCorrect, aPenalty ) {
+    return { query: aQuery, pointsMax: aPointsMax, answers: aAnswers, correct: aCorrect, penalty: aPenalty };
+}
+
+function penalty( aPointsMax ) {
+    return 1/4*aPointsMax.value;
 }
 
 var baseAnswers = ['A', 'B', 'C', 'D', 'E', 'brak odpowiedzi'];
@@ -9,40 +13,40 @@ var pts4 = { value: 4, label: '<b>4</b> punkty' };
 var pts5 = { value: 5, label: '<b>5</b> punkty' };
 
 items3Points = [
-createItem( '<img src="images/maluch/1993/01.png" width="50%"/>', pts3, baseAnswers, 'A' ),
-createItem( '<img src="images/maluch/1993/02.png" width="50%"/>', pts3, baseAnswers, 'A' ),
-createItem( '<img src="images/maluch/1993/03.png" width="50%"/>', pts3, baseAnswers, 'A' ),
-createItem( '<img src="images/maluch/1993/04.png" width="50%"/>', pts3, baseAnswers, 'A' ),
-createItem( '<img src="images/maluch/1993/05.png" width="50%"/>', pts3, baseAnswers, 'A' ),
-createItem( '<img src="images/maluch/1993/06.png" width="50%"/>', pts3, baseAnswers, 'A' ),
-createItem( '<img src="images/maluch/1993/07.png" width="50%"/>', pts3, baseAnswers, 'A' ),
-createItem( '<img src="images/maluch/1993/08.png" width="50%"/>', pts3, baseAnswers, 'A' ),
-createItem( '<img src="images/maluch/1993/09.png" width="50%"/>', pts3, baseAnswers, 'A' ),
-createItem( '<img src="images/maluch/1993/10.png" width="50%"/>', pts3, baseAnswers, 'A' ),
-createItem( '<img src="images/maluch/1994/01.png" width="50%"/>', pts3, baseAnswers, 'A' ),
-createItem( '<img src="images/maluch/1995/01.png" width="50%"/>', pts3, baseAnswers, 'A' )];
+createItem( '<img src="images/maluch/1993/01.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts3) ),
+createItem( '<img src="images/maluch/1993/02.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts3) ),
+createItem( '<img src="images/maluch/1993/03.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts3) ),
+createItem( '<img src="images/maluch/1993/04.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts3) ),
+createItem( '<img src="images/maluch/1993/05.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts3) ),
+createItem( '<img src="images/maluch/1993/06.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts3) ),
+createItem( '<img src="images/maluch/1993/07.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts3) ),
+createItem( '<img src="images/maluch/1993/08.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts3) ),
+createItem( '<img src="images/maluch/1993/09.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts3) ),
+createItem( '<img src="images/maluch/1993/10.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts3) ),
+createItem( '<img src="images/maluch/1994/01.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts3) ),
+createItem( '<img src="images/maluch/1995/01.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts3) )];
 
 items4Points = [
- createItem( '<img src="images/maluch/1993/11.png" width="50%"/>', pts3, baseAnswers, 'A' ),
- createItem( '<img src="images/maluch/1993/12.png" width="50%"/>', pts3, baseAnswers, 'A' ),
- createItem( '<img src="images/maluch/1993/13.png" width="50%"/>', pts3, baseAnswers, 'A' ),
- createItem( '<img src="images/maluch/1993/14.png" width="50%"/>', pts3, baseAnswers, 'A' ),
- createItem( '<img src="images/maluch/1993/15.png" width="50%"/>', pts3, baseAnswers, 'A' ),
- createItem( '<img src="images/maluch/1993/16.png" width="50%"/>', pts3, baseAnswers, 'A' ),
- createItem( '<img src="images/maluch/1993/17.png" width="50%"/>', pts3, baseAnswers, 'A' ),
- createItem( '<img src="images/maluch/1993/18.png" width="50%"/>', pts3, baseAnswers, 'A' ),
- createItem( '<img src="images/maluch/1993/19.png" width="50%"/>', pts3, baseAnswers, 'A' ),
- createItem( '<img src="images/maluch/1993/20.png" width="50%"/>', pts3, baseAnswers, 'A' )];
+ createItem( '<img src="images/maluch/1993/11.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts4) ),
+ createItem( '<img src="images/maluch/1993/12.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts4) ),
+ createItem( '<img src="images/maluch/1993/13.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts4) ),
+ createItem( '<img src="images/maluch/1993/14.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts4) ),
+ createItem( '<img src="images/maluch/1993/15.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts4) ),
+ createItem( '<img src="images/maluch/1993/16.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts4) ),
+ createItem( '<img src="images/maluch/1993/17.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts4) ),
+ createItem( '<img src="images/maluch/1993/18.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts4) ),
+ createItem( '<img src="images/maluch/1993/19.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts4) ),
+ createItem( '<img src="images/maluch/1993/20.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts4) )];
 
 var items5Points = [
- createItem( '<img src="images/maluch/1993/21.png" width="50%"/>', pts3, baseAnswers, 'A' ),
- createItem( '<img src="images/maluch/1993/22.png" width="50%"/>', pts3, baseAnswers, 'A' ),
- createItem( '<img src="images/maluch/1993/23.png" width="50%"/>', pts3, baseAnswers, 'A' ),
- createItem( '<img src="images/maluch/1993/24.png" width="50%"/>', pts3, baseAnswers, 'A' ),
- createItem( '<img src="images/maluch/1993/25.png" width="50%"/>', pts3, baseAnswers, 'A' ),
- createItem( '<img src="images/maluch/1993/26.png" width="50%"/>', pts3, baseAnswers, 'A' ),
- createItem( '<img src="images/maluch/1993/27.png" width="50%"/>', pts3, baseAnswers, 'A' ),
- createItem( '<img src="images/maluch/1993/28.png" width="50%"/>', pts3, baseAnswers, 'A' ),
- createItem( '<img src="images/maluch/1993/29.png" width="50%"/>', pts3, baseAnswers, 'A' ),
- createItem( '<img src="images/maluch/1993/30.png" width="50%"/>', pts3, baseAnswers, 'A' )];
+ createItem( '<img src="images/maluch/1993/21.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts5) ),
+ createItem( '<img src="images/maluch/1993/22.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts5) ),
+ createItem( '<img src="images/maluch/1993/23.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts5) ),
+ createItem( '<img src="images/maluch/1993/24.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts5) ),
+ createItem( '<img src="images/maluch/1993/25.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts5) ),
+ createItem( '<img src="images/maluch/1993/26.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts5) ),
+ createItem( '<img src="images/maluch/1993/27.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts5) ),
+ createItem( '<img src="images/maluch/1993/28.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts5) ),
+ createItem( '<img src="images/maluch/1993/29.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts5) ),
+ createItem( '<img src="images/maluch/1993/30.png" width="50%"/>', pts3, baseAnswers, 'A', penalty(pts5) )];
 
