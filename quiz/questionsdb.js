586ac0d3d4916aa4bfa1d4925b7ac892ff26ca4c
Grzegorz Dulewicz <grzegorz.dulewicz@tieto.com>
2016-04-18 13:47:55 +0200

    Selecting correct answer from db fixed.

--- quiz/questionsdb.js
+++ quiz/questionsdb.js
@@ -51,7 +51,7 @@ function generateItems( table, yBeg, yEnd, qBeg, qEnd, points, answers, correct,
     for ( year = yBeg; year <= yEnd; ++year ) {
         for ( query = qBeg; query <= qEnd; ++query ) {
             table.push( createItem( '<img src="images/maluch/' + year + '/' + ('0' + query).slice(-2) + '.png" width="50%"/>',
-                                    points, answers, correct[year-startYear][query-qBeg], aPenalty) );
+                                    points, answers, correct[year-startYear][query-1], aPenalty) );
         }
     }
 }
