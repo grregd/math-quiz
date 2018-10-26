831a52f253cf65eab3a4f67bca5f5d28976f39c5
grregd <grregd@gmail.com>
2018-10-26 21:00:58 +0200

    refactoring

--- quiz/questionsdb.js
+++ quiz/questionsdb.js
@@ -1,71 +1,4 @@
 
-var startYearMal = 1993;
-
-correctAnswers = [
-    'CCAEEBDBCAACBDCDBEDCADAEBADBDD', // 1993
-    'BCCECCBCDBEDDCBAEDAACACCEBCBDC', // 1994
-    'BEBBBADDBDADCBCEABBCBDCBBDDBDA', // 1995
-    'CDBBBBDDDCBEACDEDBCAECBED',      // 1996
-    'CCBCBBCADECBECEBDEBBDCDDC',      // 1997
-    'CDCDCCDCECCABDBDEDBBB',    // 1998
-    'CEBCDECBEBCADADDDCCAADCA', // 1999
-    'BCDABCBCBDBDBCCCBBECCAEA', // 2000
-    'CDDCBEEECDEECBCACCDAABDE', // 2001
-    'BCDDDCABCECAEBCEABDECDED', // 2002
-    'ECDACDCCCCDACABCDEEABDAB', // 2003
-    'AACCEBEBDDBBEBECECCAABEE', // 2004
-    'CABBBDECCBBCDDEDBBADCEBE', // 2005
-    'CBDAEDBBCEADCEACBDDEBECB', // 2006
-    'CACCCECBCCACABBAEBCAABDE', // 2007
-    'CDBBDAEAEDEDECBBCADEEBCD', // 2008
-    'ECBABBCDCDABBDBAEDBDABEB', // 2009
-    'DCCCDACBEBBDDCDAECDDEBDE', // 2010
-    'AACEBADBCBBCEADCACCCEDAE', // 2011
-    'ADBCBECDECECBCBDDDEEEDCD', // 2012
-    'DDBCDBEBEDDAEBBDCBDCEBBC', // 2013
-    'DDDAAEEEBEEBDBCBCCDBDBDD', // 2014
-    'EAECEEBDADBBCBDCACADECEC', // 2015
-    'EEADDCBECSBBADACEDEEBDBC'  // 2016
-];
-
-var startYearZak = 2001;
-
-correctAnswersZak = [
-    'BCCABCBABCBCBAABDB', // TEST1 2001
-    'CCACCCBBBBDBBCBBBD', // TEST2 2002
-    'CDDBACBBDBABCBCABC', // TEST3 2003
-    'ADCDCCBBBCCABCBCDB', // TEST4 2004
-    'CACBDADCCDBDABAADD', // TEST5 2005
-    'BABCBABABCAADABABD', // TEST6 2006 
-    'BCDBBBACBACBCBCBAB', // 2007  
-    'CDCADCADDABCBCBDCB', // 2008  
-    'DBCDBACDDBBCBDADCB', // 2009
-    'CCCDBCDCBBBACCCAAB', // 2010
-    'CBCADBBABCADDDBADB', // 2011
-    'DDCCBBECABADBBBEDECCE', // 2012
-    'CDBBDCBBBDEBEECDCDDDD', // 2013
-    'BBDBCEDEADBBDDDDDEEAD', // 2014
-    'ABCBBECDCCEEDEDDCDABE', // 2015
-    'DDDBDCEEAEDBCEEBEBADB' // 2016
-];
-
-var startYearSkowronek = 2005;
-
-correctAnswersSkowronek = [
-    'EDDCCCCBBBDEBADAACCBDABDCBECCB', // 2005
-    'ADDAADABBBDCEDACAABDBDDBBCCDDD' // 2006
-];
-
-var startYearJerzyk = 2005;
-
-correctAnswersJerzyk = [
-    'DBBCCBCCBDEDEECBCEEEDEDECDACDC' // 2005
-];
-
-var startYearJaskolka = 2005;
-correctAnswersJaskolka = [
-    'ACADABCBEBBDCDEBCCABCECACCDECB' // 2005
-];
 
 function createItem( aQuery, aPointsMax, aAnswers, aCorrect, aPenalty ) {
     return { query: aQuery, pointsMax: aPointsMax, answers: aAnswers, correct: aCorrect, penalty: aPenalty };
@@ -108,91 +41,149 @@ var basePath = "../../images/";
 allItems = new Map();
 generate = new Map();
 
-// maluch
+
 generate["maluch"] = function() {
+    var startYear = 1993;
+
+    correctAnswers = [
+        'CCAEEBDBCAACBDCDBEDCADAEBADBDD', // 1993
+        'BCCECCBCDBEDDCBAEDAACACCEBCBDC', // 1994
+        'BEBBBADDBDADCBCEABBCBDCBBDDBDA', // 1995
+        'CDBBBBDDDCBEACDEDBCAECBED',      // 1996
+        'CCBCBBCADECBECEBDEBBDCDDC',      // 1997
+        'CDCDCCDCECCABDBDEDBBB',    // 1998
+        'CEBCDECBEBCADADDDCCAADCA', // 1999
+        'BCDABCBCBDBDBCCCBBECCAEA', // 2000
+        'CDDCBEEECDEECBCACCDAABDE', // 2001
+        'BCDDDCABCECAEBCEABDECDED', // 2002
+        'ECDACDCCCCDACABCDEEABDAB', // 2003
+        'AACCEBEBDDBBEBECECCAABEE', // 2004
+        'CABBBDECCBBCDDEDBBADCEBE', // 2005
+        'CBDAEDBBCEADCEACBDDEBECB', // 2006
+        'CACCCECBCCACABBAEBCAABDE', // 2007
+        'CDBBDAEAEDEDECBBCADEEBCD', // 2008
+        'ECBABBCDCDABBDBAEDBDABEB', // 2009
+        'DCCCDACBEBBDDCDAECDDEBDE', // 2010
+        'AACEBADBCBBCEADCACCCEDAE', // 2011
+        'ADBCBECDECECBCBDDDEEEDCD', // 2012
+        'DDBCDBEBEDDAEBBDCBDCEBBC', // 2013
+        'DDDAAEEEBEEBDBCBCCDBDBDD', // 2014
+        'EAECEEBDADBBCBDCACADECEC', // 2015
+        'EEADDCBECSBBADACEDEEBDBC'  // 2016
+    ];
+    
     items3Points = new Array();
     items4Points = new Array();
     items5Points = new Array();
     
-    generateItems( items3Points, 1993, 1995, 1, 10, pts3, baseAnswers, correctAnswers, penalty(pts3, baseAnswers.length-1), basePath + "maluch/", startYearMal );
-    generateItems( items3Points, 1996, 1997, 1, 9,  pts3, baseAnswers, correctAnswers, penalty(pts3, baseAnswers.length-1), basePath + "maluch/", startYearMal );
-    generateItems( items3Points, 1998, 1998, 1, 7,  pts3, baseAnswers, correctAnswers, penalty(pts3, baseAnswers.length-1), basePath + "maluch/", startYearMal );
-    generateItems( items3Points, 1999, 2016, 1, 8,  pts3, baseAnswers, correctAnswers, penalty(pts3, baseAnswers.length-1), basePath + "maluch/", startYearMal );
+    generateItems( items3Points, 1993, 1995, 1, 10, pts3, baseAnswers, correctAnswers, penalty(pts3, baseAnswers.length-1), basePath + "maluch/", startYear );
+    generateItems( items3Points, 1996, 1997, 1, 9,  pts3, baseAnswers, correctAnswers, penalty(pts3, baseAnswers.length-1), basePath + "maluch/", startYear );
+    generateItems( items3Points, 1998, 1998, 1, 7,  pts3, baseAnswers, correctAnswers, penalty(pts3, baseAnswers.length-1), basePath + "maluch/", startYear );
+    generateItems( items3Points, 1999, 2016, 1, 8,  pts3, baseAnswers, correctAnswers, penalty(pts3, baseAnswers.length-1), basePath + "maluch/", startYear );
     
-    generateItems( items4Points, 1993, 1995, 11, 20, pts4, baseAnswers, correctAnswers, penalty(pts4, baseAnswers.length-1), basePath + "maluch/", startYearMal );
-    generateItems( items4Points, 1996, 1997, 10, 18, pts4, baseAnswers, correctAnswers, penalty(pts4, baseAnswers.length-1), basePath + "maluch/", startYearMal );
-    generateItems( items4Points, 1998, 1998, 8,  14, pts4, baseAnswers, correctAnswers, penalty(pts4, baseAnswers.length-1), basePath + "maluch/", startYearMal );
-    generateItems( items4Points, 1999, 2016, 9,  16, pts4, baseAnswers, correctAnswers, penalty(pts4, baseAnswers.length-1), basePath + "maluch/", startYearMal );
+    generateItems( items4Points, 1993, 1995, 11, 20, pts4, baseAnswers, correctAnswers, penalty(pts4, baseAnswers.length-1), basePath + "maluch/", startYear );
+    generateItems( items4Points, 1996, 1997, 10, 18, pts4, baseAnswers, correctAnswers, penalty(pts4, baseAnswers.length-1), basePath + "maluch/", startYear );
+    generateItems( items4Points, 1998, 1998, 8,  14, pts4, baseAnswers, correctAnswers, penalty(pts4, baseAnswers.length-1), basePath + "maluch/", startYear );
+    generateItems( items4Points, 1999, 2016, 9,  16, pts4, baseAnswers, correctAnswers, penalty(pts4, baseAnswers.length-1), basePath + "maluch/", startYear );
     
-    generateItems( items5Points, 1993, 1995, 21, 30, pts5, baseAnswers, correctAnswers, penalty(pts5, baseAnswers.length-1), basePath + "maluch/", startYearMal );
-    generateItems( items5Points, 1996, 1997, 19, 25, pts5, baseAnswers, correctAnswers, penalty(pts5, baseAnswers.length-1), basePath + "maluch/", startYearMal );
-    generateItems( items5Points, 1998, 1998, 15, 21, pts5, baseAnswers, correctAnswers, penalty(pts5, baseAnswers.length-1), basePath + "maluch/", startYearMal );
-    generateItems( items5Points, 1999, 2016, 17, 24, pts5, baseAnswers, correctAnswers, penalty(pts5, baseAnswers.length-1), basePath + "maluch/", startYearMal );
+    generateItems( items5Points, 1993, 1995, 21, 30, pts5, baseAnswers, correctAnswers, penalty(pts5, baseAnswers.length-1), basePath + "maluch/", startYear );
+    generateItems( items5Points, 1996, 1997, 19, 25, pts5, baseAnswers, correctAnswers, penalty(pts5, baseAnswers.length-1), basePath + "maluch/", startYear );
+    generateItems( items5Points, 1998, 1998, 15, 21, pts5, baseAnswers, correctAnswers, penalty(pts5, baseAnswers.length-1), basePath + "maluch/", startYear );
+    generateItems( items5Points, 1999, 2016, 17, 24, pts5, baseAnswers, correctAnswers, penalty(pts5, baseAnswers.length-1), basePath + "maluch/", startYear );
     
     allItems["maluch"] = [ items3Points, items4Points, items5Points ];
 }
 
 
-
-// żaczek 
 generate["zaczek"] = function() {
+    var startYear = 2001;
+
+    correctAnswers = [
+        'BCCABCBABCBCBAABDB', // TEST1 2001
+        'CCACCCBBBBDBBCBBBD', // TEST2 2002
+        'CDDBACBBDBABCBCABC', // TEST3 2003
+        'ADCDCCBBBCCABCBCDB', // TEST4 2004
+        'CACBDADCCDBDABAADD', // TEST5 2005
+        'BABCBABABCAADABABD', // TEST6 2006 
+        'BCDBBBACBACBCBCBAB', // 2007  
+        'CDCADCADDABCBCBDCB', // 2008  
+        'DBCDBACDDBBCBDADCB', // 2009
+        'CCCDBCDCBBBACCCAAB', // 2010
+        'CBCADBBABCADDDBADB', // 2011
+        'DDCCBBECABADBBBEDECCE', // 2012
+        'CDBBDCBBBDEBEECDCDDDD', // 2013
+        'BBDBCEDEADBBDDDDDEEAD', // 2014
+        'ABCBBECDCCEEDEDDCDABE', // 2015
+        'DDDBDCEEAEDBCEEBEBADB' // 2016
+    ];
+    
+        
     items3Points = new Array();
     items4Points = new Array();
     items5Points = new Array();
 
-    generateItems( items3Points, 2001, 2011, 1, 6, pts3, baseAnswers4, correctAnswersZak, penalty(pts3, baseAnswers4.length-1), basePath + "zaczek/", startYearZak );
-    generateItems( items3Points, 2012, 2016, 1, 7, pts3, baseAnswers, correctAnswersZak, penalty(pts3, baseAnswers.length-1), basePath + "zaczek/", startYearZak );
+    generateItems( items3Points, 2001, 2011, 1, 6, pts3, baseAnswers4, correctAnswers, penalty(pts3, baseAnswers4.length-1), basePath + "zaczek/", startYear );
+    generateItems( items3Points, 2012, 2016, 1, 7, pts3, baseAnswers,  correctAnswers, penalty(pts3, baseAnswers.length-1), basePath + "zaczek/", startYear );
 
-    generateItems( items4Points, 2001, 2011, 7, 12, pts4, baseAnswers4, correctAnswersZak, penalty(pts4, baseAnswers4.length-1), basePath + "zaczek/", startYearZak );
-    generateItems( items4Points, 2012, 2016, 8, 14, pts4, baseAnswers, correctAnswersZak, penalty(pts4, baseAnswers.length-1), basePath + "zaczek/", startYearZak );
+    generateItems( items4Points, 2001, 2011, 7, 12, pts4, baseAnswers4, correctAnswers, penalty(pts4, baseAnswers4.length-1), basePath + "zaczek/", startYear );
+    generateItems( items4Points, 2012, 2016, 8, 14, pts4, baseAnswers,  correctAnswers, penalty(pts4, baseAnswers.length-1), basePath + "zaczek/", startYear );
 
-    generateItems( items5Points, 2001, 2011, 13, 18, pts5, baseAnswers4, correctAnswersZak, penalty(pts5, baseAnswers4.length-1), basePath + "zaczek/", startYearZak );
-    generateItems( items5Points, 2012, 2015, 15, 21, pts5, baseAnswers, correctAnswersZak, penalty(pts5, baseAnswers.length-1), basePath + "zaczek/", startYearZak );
+    generateItems( items5Points, 2001, 2011, 13, 18, pts5, baseAnswers4, correctAnswers, penalty(pts5, baseAnswers4.length-1), basePath + "zaczek/", startYear );
+    generateItems( items5Points, 2012, 2015, 15, 21, pts5, baseAnswers,  correctAnswers, penalty(pts5, baseAnswers.length-1), basePath + "zaczek/", startYear );
 
     allItems["zaczek"] = [ items3Points, items4Points, items5Points ];
 }
 
-// skowronek 
 generate["skowronek"] = function() {
+    var startYear = 2005;
+
+    correctAnswers = [
+        'EDDCCCCBBBDEBADAACCBDABDCBECCB', // 2005
+        'ADDAADABBBDCEDACAABDBDDBBCCDDD' // 2006
+    ];
+
     items3Points = new Array();
     items4Points = new Array();
     items5Points = new Array();
 
-    generateItems( items3Points, 2005, 2006, 1, 10, pts3, baseAnswers, correctAnswersSkowronek, penalty(pts3, baseAnswers.length-1), basePath + "skowronek/", startYearSkowronek );
-
-    generateItems( items4Points, 2005, 2006, 11, 20, pts4, baseAnswers, correctAnswersSkowronek, penalty(pts4, baseAnswers.length-1), basePath + "skowronek/", startYearSkowronek );
-
-    generateItems( items5Points, 2005, 2006, 21, 30, pts5, baseAnswers, correctAnswersSkowronek, penalty(pts5, baseAnswers.length-1), basePath + "skowronek/", startYearSkowronek );
+    generateItems( items3Points, 2005, 2006,  1, 10, pts3, baseAnswers, correctAnswers, penalty(pts3, baseAnswers.length-1), basePath + "skowronek/", startYear );
+    generateItems( items4Points, 2005, 2006, 11, 20, pts4, baseAnswers, correctAnswers, penalty(pts4, baseAnswers.length-1), basePath + "skowronek/", startYear );
+    generateItems( items5Points, 2005, 2006, 21, 30, pts5, baseAnswers, correctAnswers, penalty(pts5, baseAnswers.length-1), basePath + "skowronek/", startYear );
 
     allItems["skowronek"] = [ items3Points, items4Points, items5Points ];
 }
 
-// jerzyk
 generate["jerzyk"] = function() {
+    var startYear = 2005;
+
+    correctAnswers = [
+        'DBBCCBCCBDEDEECBCEEEDEDECDACDC' // 2005
+    ];
+    
     items3Points = new Array();
     items4Points = new Array();
     items5Points = new Array();
 
-    generateItems( items3Points, 2005, 2005, 1, 10, pts3, baseAnswers, correctAnswersJerzyk, penalty(pts3, baseAnswers.length-1), basePath + "jerzyk/", startYearJerzyk );
-
-    generateItems( items4Points, 2005, 2005, 11, 20, pts4, baseAnswers, correctAnswersJerzyk, penalty(pts4, baseAnswers.length-1), basePath + "jerzyk/", startYearJerzyk );
-
-    generateItems( items5Points, 2005, 2005, 21, 30, pts5, baseAnswers, correctAnswersJerzyk, penalty(pts5, baseAnswers.length-1), basePath + "jerzyk/", startYearJerzyk );
+    generateItems( items3Points, 2005, 2005,  1, 10, pts3, baseAnswers, correctAnswers, penalty(pts3, baseAnswers.length-1), basePath + "jerzyk/", startYear );
+    generateItems( items4Points, 2005, 2005, 11, 20, pts4, baseAnswers, correctAnswers, penalty(pts4, baseAnswers.length-1), basePath + "jerzyk/", startYear );
+    generateItems( items5Points, 2005, 2005, 21, 30, pts5, baseAnswers, correctAnswers, penalty(pts5, baseAnswers.length-1), basePath + "jerzyk/", startYear );
 
     allItems["jerzyk"] = [ items3Points, items4Points, items5Points ];
 }
 
-// jerzyk
 generate["jaskolka"] = function() {
+    var startYear = 2005;
+    correctAnswers = [
+        'ACADABCBEBBDCDEBCCABCECACCDECB' // 2005
+    ];
+    
     items3Points = new Array();
     items4Points = new Array();
     items5Points = new Array();
 
-    generateItems( items3Points, 2005, 2005, 1, 10, pts3, baseAnswers, correctAnswersJaskolka, penalty(pts3, baseAnswers.length-1), basePath + "jaskolka/", startYearJaskolka );
-
-    generateItems( items4Points, 2005, 2005, 11, 20, pts4, baseAnswers, correctAnswersJaskolka, penalty(pts4, baseAnswers.length-1), basePath + "jaskolka/", startYearJaskolka );
-
-    generateItems( items5Points, 2005, 2005, 21, 30, pts5, baseAnswers, correctAnswersJaskolka, penalty(pts5, baseAnswers.length-1), basePath + "jaskolka/", startYearJaskolka );
+    generateItems( items3Points, 2005, 2005,  1, 10, pts3, baseAnswers, correctAnswers, penalty(pts3, baseAnswers.length-1), basePath + "jaskolka/", startYear );
+    generateItems( items4Points, 2005, 2005, 11, 20, pts4, baseAnswers, correctAnswers, penalty(pts4, baseAnswers.length-1), basePath + "jaskolka/", startYear );
+    generateItems( items5Points, 2005, 2005, 21, 30, pts5, baseAnswers, correctAnswers, penalty(pts5, baseAnswers.length-1), basePath + "jaskolka/", startYear );
 
     allItems["jaskolka"] = [ items3Points, items4Points, items5Points ];
 }
